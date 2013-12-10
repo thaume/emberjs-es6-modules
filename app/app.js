33eb59b2899873116bf2c258aff9caf236105c7e
Tom Coquereau <tom@thau.me>
2013-12-10 18:38:23 +0100

    Initial commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ import loadInitializers from 'ember/load-initializers';
 
 var App = Ember.Application.extend({
   modulePrefix: 'appkit', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver['default'],
+  rootElement: "#target"
 });
 
 loadInitializers(App, 'appkit');
