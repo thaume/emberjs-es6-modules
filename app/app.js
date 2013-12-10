6ba02f8ea30a61feafeb8f76205382bf86b6efbc
Tom Coquereau <tom@thau.me>
2013-12-10 18:38:23 +0100

    Initial commit

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ var App = Ember.Application.extend({
   LOG_TRANSITIONS_INTERNAL: true,
   LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'appkit', // TODO: loaded via config
-  Resolver: Resolver['default']
+  Resolver: Resolver['default'],
+  rootElement: "#target"
 });
 
 Ember.RSVP.configure('onerror', function(error) {
