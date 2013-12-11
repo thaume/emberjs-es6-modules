733cd852424044430d2c047f447d686e12e5975f
Tom Coquereau <tom@thau.me>
2013-12-12 00:31:37 +0100

    Fix mail route useless import

--- app/routes/mail.js
+++ app/routes/mail.js
@@ -1,8 +1,6 @@
 // Mail route
 // ==========
 
-import Mailbox from 'appkit/models/mailbox';
-
 var MailRoute = Em.Route.extend({
   model: function(params) {
     return this.modelFor('mailbox').messages.findBy('id', params.message_id);
