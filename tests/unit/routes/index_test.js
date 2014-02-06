f60d5c7a21a4df0cc6be8fcd70282a5661bb4a53
Tom Coquereau <tom@thau.me>
2014-02-06 18:17:52 +0100

    Add support for tests

--- tests/unit/routes/index_test.js
+++ tests/unit/routes/index_test.js
@@ -1,13 +1,10 @@
 import { test , moduleFor } from 'appkit/tests/helpers/module_for';
 
-import Index from 'appkit/routes/index';
+import Index from 'appkit/routes/application';
 
-moduleFor('route:index', "Unit - IndexRoute");
+describe('route:application :: unit', function(){
+  it('should pass', function(){
 
-test("it exists", function(){
-  ok(this.subject() instanceof Index);
-});
+  });
 
-test("#model", function(){
-  deepEqual(this.subject().model(), ['red', 'yellow', 'blue']);
 });
