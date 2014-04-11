f986bbff2c26f6af321999313c877936f59ce348
Stefan Penner <stefan.penner@gmail.com>
2014-04-11 08:34:15 -0400

    `Resolver['default']` is no longer needed, `Resolver` is already what we need.

--- app/app.js
+++ app/app.js
@@ -7,8 +7,7 @@ var App = Ember.Application.extend({
   LOG_TRANSITIONS_INTERNAL: true,
   LOG_VIEW_LOOKUPS: true,
   modulePrefix: 'appkit', // TODO: loaded via config
-  Resolver: Resolver['default'],
-  rootElement: "#target"
+  Resolver: Resolver
 });
 
 export default App;
